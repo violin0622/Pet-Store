edcde2463126d7d7b0050c0b4972d4bde75b02b8
Violin <violin0622@outlook.com>
2022-11-03 11:55:35 +0800

    feat(bin/client.rs): 最小客户端实现，检查服务健康状态

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,23 @@
-fn main() {
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+use crate::pet_store::{pet_store_client::PetStoreClient, HealthyRequest};
+use tonic::Request;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
+    let mut client = PetStoreClient::connect("http://[::1]:8000").await?;
+
+    client
+        .healthy(Request::new(HealthyRequest {}))
+        .await
+        .expect("Error detect service healthy");
+
+    println!("Server is healthy!");
+    Ok(())
+}
+
+pub mod pet_store {
+    tonic::include_proto!("pet_store");
 }
