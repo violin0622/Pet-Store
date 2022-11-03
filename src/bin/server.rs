91b95ccc4cc3d0f12b18641f9d5daab45e0c2811
Violin <violin0622@outlook.com>
2022-11-03 11:54:47 +0800

    feat(bin/server.rs): 服务端实现 Healthy 入口， 并整理导入语句

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -3,8 +3,8 @@
 #![allow(unused_variables)]
 use crate::pet_store::pet_store_server::{PetStore, PetStoreServer};
 use tokio_stream::wrappers::ReceiverStream;
-use tonic::transport::Server;
-use tonic::Status;
+use tonic::{transport::Server, Request, Response, Status, Streaming};
+use tonic_reflection::server::Builder;
 
 mod pet_store {
     tonic::include_proto!("pet_store");
@@ -15,15 +15,17 @@ mod pet_store {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
-    let reflection = tonic_reflection::server::Builder::configure()
+    let reflection = Builder::configure()
         .register_encoded_file_descriptor_set(pet_store::REFLECTION)
         .build()?;
     let pet_store_svc = PetStoreServer::new(PetStoreServiceServer);
+
     Server::builder()
         .add_service(reflection)
         .add_service(pet_store_svc)
         .serve("[::1]:8000".parse()?)
         .await?;
+
     Ok(())
 }
 
@@ -31,19 +33,19 @@ pub struct PetStoreServiceServer;
 
 #[tonic::async_trait]
 impl PetStore for PetStoreServiceServer {
-    type PulseStream = ReceiverStream<Result<pet_store::PulseResponse, tonic::Status>>;
+    type PulseStream = ReceiverStream<Result<pet_store::PulseResponse, Status>>;
 
     async fn pulse(
         &self,
-        request: tonic::Request<tonic::Streaming<pet_store::PulseRequest>>,
-    ) -> Result<tonic::Response<Self::PulseStream>, tonic::Status> {
+        request: Request<Streaming<pet_store::PulseRequest>>,
+    ) -> Result<Response<Self::PulseStream>, Status> {
         Err(Status::unimplemented("Unimplemented!"))
     }
 
     async fn healthy(
         &self,
-        request: tonic::Request<pet_store::HealthyRequest>,
-    ) -> Result<tonic::Response<()>, tonic::Status> {
-        Err(Status::unimplemented("Unimplemented!"))
+        request: Request<pet_store::HealthyRequest>,
+    ) -> Result<Response<()>, Status> {
+        Ok(Response::new(()))
     }
 }
