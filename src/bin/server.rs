164483aa81a7fd39e35e87fac7a1b1428f3c736f
Violin <violin0622@outlook.com>
2022-12-30 14:46:35 +0800

    refactor(svc): 将 PetStore 服务的实现代码迁移至 svc 模块中

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,15 +1,5 @@
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use pet_store::{
-    google::r#type::Date,
-    pet_store::pet_store_server::{PetStore, PetStoreServer},
-    pet_store::*,
-};
-use tokio::sync::mpsc;
-use tokio::time::{self, Duration};
-use tokio_stream::{wrappers::ReceiverStream, StreamExt};
-use tonic::{transport::Server, Request, Response, Status, Streaming};
+use pet_store::{pet_store::pet_store_server::PetStoreServer, svc::PetStoreService};
+use tonic::transport::Server;
 use tonic_reflection::server::Builder;
 
 mod reflection {
@@ -23,7 +13,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let reflection = Builder::configure()
         .register_encoded_file_descriptor_set(reflection::REFLECTION)
         .build()?;
-    let pet_store_svc = PetStoreServer::new(PetStoreServiceServer);
+    let pet_store_svc = PetStoreServer::new(PetStoreService::new());
 
     Server::builder()
         .add_service(reflection)
@@ -32,87 +22,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     Ok(())
-}
-
-pub struct PetStoreServiceServer;
-
-#[tonic::async_trait]
-impl PetStore for PetStoreServiceServer {
-    type PulseStream = ReceiverStream<Result<PulseResponse, Status>>;
-    async fn pulse(
-        &self,
-        request: Request<Streaming<PulseRequest>>,
-    ) -> Result<Response<Self::PulseStream>, Status> {
-        Err(Status::unimplemented("Unimplemented!"))
-    }
-
-    async fn healthy(&self, request: Request<HealthyRequest>) -> Result<Response<()>, Status> {
-        Ok(Response::new(()))
-    }
-
-    type ListPetsStream = ReceiverStream<Result<ListPetsResponse, Status>>;
-    async fn list_pets(
-        &self,
-        request: Request<ListPetsRequest>,
-    ) -> Result<Response<Self::ListPetsStream>, Status> {
-        let (tx, rx) = mpsc::channel(10);
-        let mut counter = 0;
-        let mut interval = time::interval(Duration::from_millis(100));
-        tokio::spawn(async move {
-            while counter < 10 {
-                interval.tick().await;
-                counter += 1;
-                tx.send(Ok(ListPetsResponse {
-                    id: counter,
-                    name: format!("Pet No.{counter}"),
-                    species: String::from("Dog"),
-                    variety: String::from("Dachshund"),
-                    // birthday: None::<()>,
-                    birthday: Some(Date {
-                        year: 2000,
-                        month: 12,
-                        day: 30,
-                    }),
-                    comment: String::from(""),
-                }))
-                .await
-                .unwrap();
-            }
-        });
-
-        Ok(Response::new(ReceiverStream::new(rx)))
-        // Err(Status::unimplemented("Unimplemented!"))
-    }
-
-    async fn register_pet(
-        &self,
-        request: tonic::Request<RegisterPetRequest>,
-    ) -> Result<tonic::Response<RegisterPetResponse>, tonic::Status> {
-        Err(Status::unimplemented("Unimplemented!"))
-    }
-
-    async fn unregister_pet(
-        &self,
-        request: tonic::Request<UnregisterPetRequest>,
-    ) -> Result<tonic::Response<()>, tonic::Status> {
-        Err(Status::unimplemented("Unimplemented!"))
-    }
-    async fn view_pet(
-        &self,
-        request: tonic::Request<ViewPetRequest>,
-    ) -> Result<tonic::Response<ViewPetResponse>, tonic::Status> {
-        Err(Status::unimplemented("Unimplemented!"))
-    }
-    async fn update_pet(
-        &self,
-        request: tonic::Request<UpdatePetRequest>,
-    ) -> Result<tonic::Response<()>, tonic::Status> {
-        Err(Status::unimplemented("Unimplemented!"))
-    }
-    async fn modify_pet(
-        &self,
-        request: tonic::Request<ModifyPetRequest>,
-    ) -> Result<tonic::Response<()>, tonic::Status> {
-        Err(Status::unimplemented("Unimplemented!"))
-    }
 }
---
+++ src/svc/service.rs
@@ -0,0 +1,101 @@
+#![allow(unused_variables)]
+
+use crate::{
+    dao::new_connection,
+    google::r#type::Date,
+    pet_store::{pet_store_server::PetStore, *},
+};
+use diesel::PgConnection;
+use tokio::sync::mpsc;
+use tokio::time::{self, Duration};
+use tokio_stream::wrappers::ReceiverStream;
+use tonic::{Request, Response, Status, Streaming};
+
+pub struct PetStoreService {}
+
+impl PetStoreService {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+#[tonic::async_trait]
+impl PetStore for PetStoreService {
+    type PulseStream = ReceiverStream<Result<PulseResponse, Status>>;
+    async fn pulse(
+        &self,
+        request: Request<Streaming<PulseRequest>>,
+    ) -> Result<Response<Self::PulseStream>, Status> {
+        Err(Status::unimplemented("Unimplemented!"))
+    }
+
+    async fn healthy(&self, request: Request<HealthyRequest>) -> Result<Response<()>, Status> {
+        Ok(Response::new(()))
+    }
+
+    type ListPetsStream = ReceiverStream<Result<ListPetsResponse, Status>>;
+    async fn list_pets(
+        &self,
+        request: Request<ListPetsRequest>,
+    ) -> Result<Response<Self::ListPetsStream>, Status> {
+        let (tx, rx) = mpsc::channel(10);
+        let mut counter = 0;
+        let mut interval = time::interval(Duration::from_millis(100));
+        tokio::spawn(async move {
+            while counter < 10 {
+                interval.tick().await;
+                counter += 1;
+                tx.send(Ok(ListPetsResponse {
+                    id: counter,
+                    name: format!("Pet No.{counter}"),
+                    species: String::from("Dog"),
+                    variety: String::from("Dachshund"),
+                    birthday: None::<Date>,
+                    // birthday: Some(Date {
+                    //     year: 2000,
+                    //     month: 12,
+                    //     day: 30,
+                    // }),
+                    comment: String::from(""),
+                }))
+                .await
+                .unwrap();
+            }
+        });
+
+        Ok(Response::new(ReceiverStream::new(rx)))
+        // Err(Status::unimplemented("Unimplemented!"))
+    }
+
+    async fn register_pet(
+        &self,
+        request: tonic::Request<RegisterPetRequest>,
+    ) -> Result<tonic::Response<RegisterPetResponse>, tonic::Status> {
+        Err(Status::unimplemented("Unimplemented!"))
+    }
+
+    async fn unregister_pet(
+        &self,
+        request: tonic::Request<UnregisterPetRequest>,
+    ) -> Result<tonic::Response<()>, tonic::Status> {
+        Err(Status::unimplemented("Unimplemented!"))
+    }
+    async fn view_pet(
+        &self,
+        request: tonic::Request<ViewPetRequest>,
+    ) -> Result<tonic::Response<ViewPetResponse>, tonic::Status> {
+        Err(Status::unimplemented("Unimplemented!"))
+    }
+    async fn update_pet(
+        &self,
+        request: tonic::Request<UpdatePetRequest>,
+    ) -> Result<tonic::Response<()>, tonic::Status> {
+        Err(Status::unimplemented("Unimplemented!"))
+    }
+    async fn modify_pet(
+        &self,
+        request: tonic::Request<ModifyPetRequest>,
+    ) -> Result<tonic::Response<()>, tonic::Status> {
+        Err(Status::unimplemented("Unimplemented!"))
+    }
+}
